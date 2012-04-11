26e0c0be7dc1c1dd11ffc39756129a2fbc52aaef
Maxim Pustynnikov <pustynnikov@cupid.com>
2012-04-11 14:50:21 +0300

    3rd test refactoring

--- 3/main.js
+++ 3/main.js
@@ -1,11 +1,16 @@
 window.onload = function() {
-    document.getElementById('color_input').onkeyup = function() {
-        var colorRegexp  = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$|^[a-zA-z]{3,}$)/i;
-        if(!colorRegexp.test(this.value)) {
-            return false;
+    var result;
+
+    document.getElementById('color-code').onkeyup = function() {
+        var colorRegExp  = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$|^[a-zA-z]{3,}$)/i;
+
+        if(!colorRegExp.test(this.value)) {
+            result = 'Invalid color';
+        } else {
+            result = 'Current color is <b>' + this.value + '</b>';
+            document.getElementById('color-appearance').style.background = this.value;
         }
 
-        document.getElementById('color_appearance').style.background = this.value;
-        document.getElementById('color_current').innerHTML = 'Current color is <b>' + this.value + '</b>';
+        document.getElementById('info').innerHTML = result;
     };
 };
